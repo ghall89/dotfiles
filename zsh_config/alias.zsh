c612a8dc8f671d4fd0053e4d20db8172fa944f31
Graham Hall <graham@ghall.dev>
2024-04-28 15:37:38 -0400

    update and clean up some configs

--- zsh_config/alias.zsh
+++ zsh_config/alias.zsh
@@ -1,12 +1,27 @@
-alias config="bbedit ~/dotfiles"
+# overrides
+alias ls="eza"
+alias cd="z"
+
+# developer stuff
 alias nv="open -a Nova"
+alias runx="npx concurrently \"serverless offline\" \"nx dev frontend\""
+
+# git
 alias ptob="gh pr create -B beta"
 alias prune-git="git remote prune origin"
+
+# ssh
+alias macmini="ssh admin@Memory-Alpha.local"
+
+# shell scripts
 alias dev="sh ~/dotfiles/scripts/run-dev.sh"
 alias lint="sh ~/dotfiles/scripts/run-lint.sh"
 alias commit="sh ~/dotfiles/scripts/git-commit-push.sh"
-alias macmini="ssh admin@Memory-Alpha.local"
-alias ls="eza"
-alias cd="z"
-alias runx="npx concurrently \"serverless offline\" \"nx dev frontend\""
-alias brewfile="brew bundle dump --file=~/dotfiles/Brewfile --force"
+
+# homebrew
+alias brewfile="brew bundle dump --file=~/dotfiles/Brewfile --force --"
+alias installbrewfile="brew bundle install --file=~/dotfiles/Brewfile"
+
+# dotfiles
+alias config="bbedit ~/dotfiles"
+alias linkdfs="sh ~/dotfiles/symlink_dotfiles.zsh"
